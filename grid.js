13a2fdec86a9a9a2dc539196ce5f711866cbfd88
sookool99 <shawnmacburnie@gmail.com>
2015-09-17 16:39:06 -0300

    completed basic functionality

---
+++ grid.js
@@ -0,0 +1,64 @@
+(function (window) {
+    'use strict';
+    if (!window.globals) {
+        window.globals = {};
+    }
+    var Grid = {
+        size: {
+            x: 100,
+            y: 100
+        },
+        padding: 0.5,
+        points: [],
+        create: function (size) {
+            var grid = Object.create(this);
+            if (size) {
+                grid.size = size;
+            }
+            grid.generateGrid();
+            return grid;
+        },
+        at: function (point) {
+            return this.points[point.x][point.y];
+        },
+        generateGrid: function () {
+            for (var x = 0; x < this.size.x; x++) {
+                var row = [];
+                for (var y = 0; y < this.size.y; y++) {
+                    row.push(0);
+                }
+                this.points.push(row);
+            }
+        },
+        update: function (position, context) {
+            if (this.points[position.x][position.y]) {
+                this.points[position.x][position.y] = 0;
+            } else {
+                this.points[position.x][position.y] = 1;
+            }
+            this.drawGrid(context);
+        },
+        drawGrid: function (context) {
+            var canvas = document.getElementById('c');
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.beginPath();
+            var height = (canvas.height - 2 * this.padding) / this.size.y,
+                width = (canvas.height - 2 * this.padding) / this.size.x;
+            for (var x = 0; x < this.size.x; x++) {
+                for (var y = 0; y < this.size.y; y++) {
+                    if (this.points[x][y] === 0) {
+                        context.rect((x + (width * x) + this.padding), (y +
+                            (height * y) + this.padding), width, height);
+                    } else {
+                        context.fillRect((x + (width * x) + this.padding), (
+                                y + (height * y) + this.padding), width,
+                            height);
+                    }
+                }
+            }
+            context.save();
+            context.stroke();
+        }
+    };
+    window.globals.Grid = Grid;
+})(window);
--- index.js
+++ index.js
@@ -1,62 +1,20 @@
 'use strict';
 
-function init() {
-    var c_canvas = document.getElementById('c');
-    var context = c_canvas.getContext('2d');
-    // drawGrid(context);
-    grid.init();
-    grid.drawGrid(context);
+function min(a, b) {
+    if (a < b) {
+    	console.log(a + ' is less then ' + b);
+        return a;
+    }
+    console.log(b + ' is less then ' + a);
+    return b;
 }
 
-function drawGrid(context, height, width) {
-    context.rect(0.5, 0.5, 10, 10);
-    // for (var x = 0.5; x < width; x += 10) {
-    //   context.moveTo(x, 0);
-    //   context.lineTo(x, height);
-    // }
-    // for (var y = 0.5; y < height; y += 10) {
-    //   context.moveTo(0, y);
-    //   context.lineTo(width, y);
-    // }
-    // context.strokeStyle = '#eee';
-    context.stroke();
+function init() {
+    var c = document.getElementById('c'),
+        shape = min(window.innerWidth, window.innerHeight);
+    c.width = shape;
+    c.height = shape;
+    var context = c.getContext('2d');
+    var game = window.globals.Game.create(context);
+    game.play(1, 50);
 }
-var grid = {
-    size: {
-        x: 100,
-        y: 100
-    },
-    points: [],
-    init: function (size) {
-        if (size) {
-            this.size = size;
-        }
-        this.generateGrid();
-    },
-    generateGrid: function () {
-        for (var x = 0; x < this.size.x; x++) {
-            var row = [];
-            for (var y = 0; y < this.size.y; y++) {
-                row.push(0);
-            }
-            this.points.push(row);
-        }
-    },
-    drawGrid: function (context) {
-        var padding = 0.5,
-            height = 10,
-            width = 10;
-        for (var x = 0; x < this.size.x; x++) {
-            for (var y = 0; y < this.size.y; y++) {
-                if (this.points[x][y] === 0) {
-                    context.rect((x + (width * x) + padding), (y + (height * y) +
-                        padding), width, height);
-                } else {
-                    context.fillRect((x + (width * x) + padding), (y + (height *
-                        y) + padding), width, height);
-                }
-            }
-        }
-        context.stroke();
-    }
-};
